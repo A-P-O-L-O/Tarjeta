9114e06ce2efb3030cfd6907313b341768c7684d
Elias Jaramillo <devJaramillo@gmail.com>
2023-10-18 20:40:57 -0500

    mejora

--- src/main/java/PQ/LOGICA/Index.java
+++ src/main/java/PQ/LOGICA/Index.java
@@ -5,40 +5,10 @@ import PQ.IGU.Validacion;
 public class Index {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
 
         Validacion validacion = new Validacion();
         validacion.setVisible(true);
         validacion.setLocationRelativeTo(null);
-=======
-        
-        
-                Personalizada creacion = new Personalizada();
-                creacion.setVisible(true);
-                creacion.setLocationRelativeTo(null);
-        //
-        ////        NoPersonalizada creacion = new NoPersonalizada();
-        ////        creacion.setVisible(true);
-        ////        creacion.setLocationRelativeTo(null);
-//
-        TarjetaPersonalizadaJpaController tarjetaController = new TarjetaPersonalizadaJpaController();
-//
-//        List<Integer> numerosDeTarjeta = tarjetaController.getNumerosTarjetaList();
-//
-//        for (Integer numeroTarjeta : numerosDeTarjeta) {
-//            System.out.println(numeroTarjeta);
-//        }e
-
-//        List<TarjetaPersonalizada> todasLasTarjetas = tarjetaController.getTodasLasTarjetas();
-//
-//        for (TarjetaPersonalizada tarjeta : todasLasTarjetas) {
-//            System.out.println("Número de tarjeta: " + tarjeta.getNombrePropietario());
-//        }
-//
-//Recarga recarga = new Recarga();
-//recarga.setVisible(true);
-//recarga.setLocationRelativeTo(null);
->>>>>>> 01c727f65bc423e333739533e9f2a7f378b9cf97
 
     }
 }
